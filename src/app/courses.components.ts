f5b2029bdfb08ae4fe1f88c05a8bd1c546c0080a
shafiqismai2022 <shafiqismail664@gmail.com>
2023-05-27 15:27:49 +0300

    templates

--- src/app/courses.components.ts
+++ src/app/courses.components.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 @Component({
     selector:'angular',
-    template:'<h2>Hellow World</h2>'
+    template:'<h2>{{getTitle()}}</h2>'
 
 })
 export class CourseComponents{
-
+title:String='Hellow every one'
+getTitle():string {
+return "This is just a test"    
+}
 }
