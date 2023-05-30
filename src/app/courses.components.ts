ea885f9ff03906d22199776f3124ebceec75fd05
shafiqismai2022 <shafiqismail664@gmail.com>
2023-05-30 10:14:19 +0300

    Bootsrap in Angular

--- src/app/courses.components.ts
+++ src/app/courses.components.ts
@@ -3,14 +3,22 @@ import { courseService } from "./course.service";
 @Component({
     selector:'angular',
     template:`<h2>{{getTitle()}}</h2>
-    <ul>
-    <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
+    <!-- <ul>
+    <li *ngFor="let course of courses">{{ course }}</li> // Dom manipulation
+    </ul> -->
+    <!-- <img src="{{imgUrl}}" alt=""> // String Interpolation is used for text-->
+    <img [src]="imgUrl" alt="People Laughing">
+    <!-- <table>
+        <tr><td [attr.colspan]="colspan"></td></tr> // attribute Not essentail to be Dom element 
+    </table> -->
+    <button class="btn btn-primary">Save</button>
     `
 
 })
 export class CourseComponents{
+    colspan:number=2;
 title:String='Hellow every one';
+imgUrl:String="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPcQULXbGZ6y7CkCDnIlP72ZVRVX8VQPBwGCFE3qtLz6RTehRX3IgqxVJ6HHHOsY7FWWo&usqp=CAU"
 courses:String[]=[];
 constructor(serv: courseService ){ //dependency
     // let serv = new courseService();
